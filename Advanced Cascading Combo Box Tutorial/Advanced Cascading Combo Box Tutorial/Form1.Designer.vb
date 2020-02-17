80090f68449805d5e746574c481cc16db723ed78
Mausam Giri <48096127+gtmgiri110@users.noreply.github.com>
2020-02-17 07:38:43 -0800

    Add files via upload

---
+++ Advanced Cascading Combo Box Tutorial/Advanced Cascading Combo Box Tutorial/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cbostates = New System.Windows.Forms.ComboBox()
+        Me.cbocities = New System.Windows.Forms.ComboBox()
+        Me.btnclose = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Trebuchet MS", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DarkBlue
+        Me.Label1.Location = New System.Drawing.Point(17, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(383, 35)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Welcome to Big Box | iTechZ"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(14, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "State: -"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(14, 130)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "City: -"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.DarkRed
+        Me.Label4.Location = New System.Drawing.Point(168, 53)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(234, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Connection State : InActive!"
+        '
+        'cbostates
+        '
+        Me.cbostates.FormattingEnabled = True
+        Me.cbostates.Location = New System.Drawing.Point(93, 91)
+        Me.cbostates.Name = "cbostates"
+        Me.cbostates.Size = New System.Drawing.Size(299, 24)
+        Me.cbostates.TabIndex = 4
+        '
+        'cbocities
+        '
+        Me.cbocities.FormattingEnabled = True
+        Me.cbocities.Location = New System.Drawing.Point(93, 130)
+        Me.cbocities.Name = "cbocities"
+        Me.cbocities.Size = New System.Drawing.Size(299, 24)
+        Me.cbocities.TabIndex = 5
+        '
+        'btnclose
+        '
+        Me.btnclose.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnclose.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclose.ForeColor = System.Drawing.Color.DarkRed
+        Me.btnclose.Location = New System.Drawing.Point(18, 174)
+        Me.btnclose.Name = "btnclose"
+        Me.btnclose.Size = New System.Drawing.Size(374, 30)
+        Me.btnclose.TabIndex = 6
+        Me.btnclose.Text = "&Close Application"
+        Me.btnclose.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(414, 216)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btnclose)
+        Me.Controls.Add(Me.cbocities)
+        Me.Controls.Add(Me.cbostates)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Cascading Comboboxes in Vb.net"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents cbostates As System.Windows.Forms.ComboBox
+    Friend WithEvents cbocities As System.Windows.Forms.ComboBox
+    Friend WithEvents btnclose As System.Windows.Forms.Button
+
+End Class
---
+++ Advanced Cascading Combo Box Tutorial/Advanced Cascading Combo Box Tutorial/Form1.vb
@@ -0,0 +1,71 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+    Dim con As New OleDbConnection
+    Dim constring As String
+    Dim cmd As New OleDbCommand
+    Dim dr As OleDbDataReader
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        constring = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|/dat.accdb"
+        With con
+
+            .ConnectionString = constring
+            .Open()
+
+        End With
+
+        Me.Label4.Text = "Connection State : Active!"
+        Me.Label4.ForeColor = Color.DarkGreen
+
+        FillStates()
+    End Sub
+
+    Public Sub FillStates()
+        With cmd
+            .Connection = con
+            .CommandType = CommandType.Text
+            .CommandText = "SELECT DISTINCT tbldat.State from tbldat"
+        End With
+
+        dr = cmd.ExecuteReader()
+        While dr.Read()
+            cbostates.Items.Add(dr("State"))
+        End While
+    End Sub
+
+    Public Sub FillCities()
+        Dim cmd1 As New OleDbCommand
+        Dim dr1 As OleDbDataReader
+
+        With cmd1
+            .Connection = con
+            .CommandType = CommandType.Text
+            .CommandText = "SELECT * from tbldat where [State] ='" & Me.cbostates.Text & "'"
+        End With
+
+        dr1 = cmd1.ExecuteReader()
+
+        While dr1.Read()
+            cbocities.Items.Add(dr1("City"))
+        End While
+    End Sub
+
+    Private Sub btnclose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnclose.Click
+        con.Close()
+        Dispose()
+
+    End Sub
+
+    Private Sub cbostates_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbostates.SelectedIndexChanged
+        cbocities.Text = "-Select-"
+        If Me.cbocities.SelectedIndex >= -1 Then
+            Me.cbocities.Items.Clear()
+            FillCities()
+        Else
+            FillCities()
+
+        End If
+        
+    End Sub
+End Class
